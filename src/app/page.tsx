5a5ac0076d5ee0ac85c20976674bad8a7a96684b
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-30 15:48:44 -0300

    chore: update sentence and add confirmation link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { links } from "@/config/links"
 
 import { cn } from "@/lib/utils"
 
-import { ExternalLink } from "lucide-react"
+import { CalendarCheckIcon, ExternalLink } from "lucide-react"
 
 export const metadata: Metadata = {
   title: "Laís e Leonardo | Lista de Presentes"
@@ -49,7 +49,7 @@ const HomePage = () => {
 
       {/* Main Content */}
       <div className="relative container mx-auto max-w-2xl px-4 py-16">
-        <header className="mb-12 text-center">
+        <header className="text-center">
           <Image
             src="/photo.jpg"
             width={250}
@@ -58,15 +58,14 @@ const HomePage = () => {
             className="mx-auto mb-4 size-36 rounded-full object-cover"
           />
 
-          <h1 className="text-brand mb-2 text-4xl md:text-5xl">
+          <h1 className="text-brand text-4xl md:text-5xl">
             Lista de Presentes
           </h1>
-          <p className="text-brand-secondary text-xl font-normal">
-            Obrigado por pensar em nós.
+          <p className="text-brand-secondary mt-2 text-xl font-normal">
+            Agradecemos a sua contribuição ♥
           </p>
         </header>
-
-        <div className="space-y-4">
+        <div className="my-6 space-y-4">
           {/* Amazon Wishlist */}
 
           {links.map((link) => {
@@ -102,6 +101,29 @@ const HomePage = () => {
             )
           })}
         </div>
+
+        <p className="text-brand-secondary mt-2 text-center text-xl font-normal">
+          Ainda não confirmou sua presença?
+        </p>
+
+        <Link
+          href="https://sites.icasei.com.br/laisechristian/pages/32050268"
+          target="_blank"
+          className="bg-opacity-50 group mt-4 block rounded-xl bg-white p-4 shadow-sm backdrop-blur-sm transition-all duration-500 select-none hover:translate-y-[-2px] hover:shadow-md"
+        >
+          <div className="flex items-center gap-4">
+            <div className="bg-opacity-10 bg-brand flex size-12 items-center justify-center rounded-full p-1.5">
+              <CalendarCheckIcon className="text-white" />
+            </div>
+
+            <div className="flex-1">
+              <h2 className="text-brand text-xl font-medium md:text-2xl">
+                Confirme aqui
+              </h2>
+            </div>
+            <ExternalLink className="text-brand-secondary h-5 w-5 opacity-0 transition-opacity group-hover:opacity-100" />
+          </div>
+        </Link>
       </div>
     </div>
   )
