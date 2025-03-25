ef0a2f33501a7aa75ac86015afd843ae58d1bb82
Christian Leonardo <whyleonardo.dev@gmail.com>
2025-03-25 00:44:21 -0300

    Atualizar o links.ts

--- src/config/links.ts
+++ src/config/links.ts
@@ -2,21 +2,9 @@ import { Icons } from "@/components/icons"
 
 export const links = [
   {
-    name: "Amazon",
-    icon: Icons.amazon,
-    href: "https://www.amazon.com.br/hz/wishlist/ls/2AHVRGQA2O797?ref_=wl_share",
-    description: "Acesse a lista de presentes da Amazon"
-  },
-  {
     name: "Magazine Luiza",
     icon: Icons.magalu,
     href: "https://www.finalfeliz.de/lais-e-leo-lista",
     description: "Acesse a lista de presentes da Magalu"
-  },
-  {
-    name: "Casas Bahia",
-    icon: Icons.casasBahia,
-    href: "https://listas.casasbahia.com.br/LAISELEONARDOLISTA",
-    description: "Acesse a lista de presentes da Casas Bahia"
   }
 ] as const
